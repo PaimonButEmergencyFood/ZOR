f32f498c1e8cbef5696cf1ed34bba3ba7b887503
pythonlimited <leonardomeitz@gmail.com>
2022-04-05 15:16:17 +0200

    more bag helper functions

--- gameserver/Logic/Bag.cs
+++ gameserver/Logic/Bag.cs
@@ -6,8 +6,17 @@ namespace ProjectZ.Logic  {
         private int _itemCount;
         private int _eBagType;
         private int _maxSlotCount;
-        public Bag() {
-            
+        public Bag(int eBagType) {
+            _clsItemVector = new List<Item>();
+            _bLoad = false;
+            _bDecreaseBattlepetStamina = false;
+            _itemCount = -1;
+            _eBagType = -1;
+            _maxSlotCount = Function.GetBagSlotMaxOpenCount((INVEN_BAG_TYPE)eBagType);
+        }
+
+        public int HasItemCount() {
+            return _itemCount;
         }
 
         public int GetBagType() {
@@ -24,6 +33,67 @@ namespace ProjectZ.Logic  {
 
         public List<Item> GetItemVector() {
             return _clsItemVector;
+        }
+
+        public bool AddItem(Item item) {
+            if (item == null) {
+                return false;
+            }
+
+            if (HasItemCount() >= _maxSlotCount) {
+                return false;
+            }
+
+            _clsItemVector.Add(item);
+            _itemCount++;
+            return true;
+        }
+
+        public Item GetItem(UInt32 ItemSeq) {
+            for (int i = 0; i < _clsItemVector.Count; i++) {
+                if (_clsItemVector[i].GetItemSeq == ItemSeq) {
+                    return _clsItemVector[i];
+                }
+            }
+
+            return null;
+        }
+
+        public Item PopItem(UInt32 ItemSeq) {
+            for (int i = 0; i < _clsItemVector.Count; i++) {
+                if (_clsItemVector[i].GetItemSeq == ItemSeq) {
+                    Item item = _clsItemVector[i];
+                    _clsItemVector.RemoveAt(i);
+                    _itemCount--;
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        public bool IsBagFull() {
+            return HasItemCount() >= _maxSlotCount;
+        }
+
+        public bool IsExistItem(UInt32 ItemSeq) {
+            for (int i = 0; i < _clsItemVector.Count; i++) {
+                if (_clsItemVector[i].GetItemSeq == ItemSeq) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsExistSameItem(Item item) {
+            for (int i = 0; i < _clsItemVector.Count; i++) {
+                if (_clsItemVector[i].SubType == item.SubType && _clsItemVector[i].Tid != item.Tid) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
