c8ff69245f5ab502d181cc4430be9be49ff89cc7
pythonlimited <leonardomeitz@gmail.com>
2022-04-12 12:17:06 +0200

    initial proxy bringup this is for "communication" with the cache, location and ifriend

---
+++ Gameserver/NProxy/Proxy.cs
@@ -0,0 +1,11 @@
+using Cache;
+using Location;
+using iFriends;
+
+namespace ProjectZ.NProxy {
+    public static class Proxy {
+        public static bool RegistUser(ref User _user) {
+            return true;
+        }
+    }
+}
